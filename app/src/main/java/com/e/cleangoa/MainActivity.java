a022bf1555e7ba0b573970c5a155dec7770469e2
Ashier Fernandes <ashierchelsea@gmail.com>
2019-09-20 03:11:48 +0530

    only last part remains

--- app/src/main/java/com/e/cleangoa/MainActivity.java
+++ app/src/main/java/com/e/cleangoa/MainActivity.java
@@ -2,13 +2,19 @@ package com.e.cleangoa;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.view.menu.MenuView;
 import androidx.appcompat.widget.Toolbar;
 import androidx.viewpager.widget.ViewPager;
+
+import android.content.ClipData;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.DragEvent;
 import android.view.MenuInflater;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.MotionEvent;
+import android.widget.Button;
 
 import com.google.android.material.tabs.TabLayout;
 
@@ -17,7 +23,7 @@ private Toolbar toolbar;
 private ViewPager viewPager;
 private ViewPagerAdapter adapter;
 private TabLayout tabLayout;
-
+private MenuItem item;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,12 +39,42 @@ private TabLayout tabLayout;
         tabLayout=findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(viewPager);
     }
-    public void setAdapter()
+//    public void adapter()
+//    {
+//        switch (viewPager.getCurrentItem())
+//        {
+//            case 1:
+//            { Button button;
+//                button=(Button)findViewById(R.id.button);
+////                button.setOnClickListener(new);
+//
+//                break;
+//            }
+//            default:
+//
+//        }
+//    }
+
+public boolean onOptionsItemSelected(MenuItem item)
+{
+    switch(item.getItemId())
     {
-        switch ((viewPager.getCurrentItem()))
-    }
-
-
+    case R.id.item0:
+    {startActivity(new Intent(this, login.class));
+        return true;}
+    case R.id.item1:
+    {  startActivity(new Intent(this, login.class));
+        return true;}
+    case R.id.item2:
+    {  startActivity(new Intent(this, termsandconditions.class));
+        return true;}
+    case R.id.item3:
+    {startActivity(new Intent(this, registration.class));
+        return true;}
+    default:
+    {  return super.onOptionsItemSelected(item);}
+}
+}
     @Override
     public boolean onCreateOptionsMenu(Menu menu){
         MenuInflater inflator=getMenuInflater();
